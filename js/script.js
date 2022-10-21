6b66ea58c2f14e984562d60bf6942047af4f1e66
victor-sp-01 <55814035+victor-sp-01@users.noreply.github.com>
2022-10-21 15:57:30 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,20 @@ let total = 0;
 function add(product, price) {
     console.log(product, price);
     products.push(product);
-    total = total + price;
+    total = total + price; 
     document.getElementById("checkout").innerHTML = `Pagar $${total}`
 }
+
+/*
+const add =(product, price)=> {
+    products.push(product)
+    total+=price
+    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+}
+
+const pay =()=> { window.alert(products.join(", \n")) } 
+
+*/
 
 function pay() {
     window.alert(products.join(", \n"));
